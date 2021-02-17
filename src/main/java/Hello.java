94b9f30170c12666700e644244e08c731bbd428e
furkan <ozdemir1furkan@gmail.com>
2021-02-17 00:39:46 -0300

    movie3 branch watching in the living room

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
         System.out.println("Hello World EU4");
         System.out.println("EU4 future SDET learning Git");
         //movie3 branch watching on the phone
+        //movie3 branch watching in the living room
     }
         // Do not touch this line
 }
