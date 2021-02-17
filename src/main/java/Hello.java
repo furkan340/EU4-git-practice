82db905bd31dd7144c7e76f6bc6aaf91992b9013
furkan <ozdemir1furkan@gmail.com>
2021-02-16 23:54:48 -0300

    master branch , spouse changed the movie location

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World EU4");
         System.out.println("EU4 future SDET learning Git");
 
     }
