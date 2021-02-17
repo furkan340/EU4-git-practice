682f05f5fdc8cb43199e48c722a4980124a9d9ff
furkan <ozdemir1furkan@gmail.com>
2021-02-16 23:07:01 -0300

    more work on featureA on new branch

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -3,6 +3,7 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work for A started");
+        System.out.println("EU4 is learning more git with review");
 
     }
 }
