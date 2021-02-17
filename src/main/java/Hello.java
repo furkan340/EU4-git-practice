008e9092237e2be202b2326c71f7d61052d6adf5
furkan <ozdemir1furkan@gmail.com>
2021-02-17 00:21:16 -0300

    movie2 branch watching on the phone for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World EU4");
         System.out.println("EU4 future SDET learning Git");
+        // movie on th ephone 10 minutes
 
     }
 }
