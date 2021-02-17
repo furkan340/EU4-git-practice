1949ef4aae4dabe8c3b7b9c2469dc65f1acdea0f
furkan <ozdemir1furkan@gmail.com>
2021-02-17 00:23:35 -0300

    master branch watching on the living room for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World EU4");
         System.out.println("EU4 future SDET learning Git");
-
     }
+        // Oh I like this movie who played it ? let me continue
 }
